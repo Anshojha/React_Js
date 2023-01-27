6fa00cb8d445a144182509f3317da47cf2ffa4ae
anshojha <anshojha24@gmail.com>
2023-01-28 01:20:22 +0530

    Added button to delete the task

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,11 +1,41 @@
+import { useState } from "react"
 import Header from "./components/Header"
-import Button from "./components/Button"
+import Tasks from "./components/Tasks"
 
 function App() {
+  const [tasks , setTasks] = useState(
+    [
+        {
+            id : 1,
+            text : 'Doctors Appointment',
+            day : 'Feb 5th at 2:30pm',
+            remainder:true,
+        },
+        {
+            id:2,
+            text : 'Meeting at school',
+            day : 'Feb 6th at 2:30pm',
+            remainder : true,
+        },
+        {
+            id : 3,
+            text : 'Food Shooping',
+            day : 'Feb 5th at 2:30pm',
+            reminder : false,
+        }
+    ]
+)
+
+//Function to delete the task
+
+const deleteTask = (id)=>{
+  setTasks(tasks.filter((task)=>task.id !==id))
+}
   return (
+    
     <div className="container">
       <Header/>
-      <Button color='green' text='Add'/>
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   )
 }
