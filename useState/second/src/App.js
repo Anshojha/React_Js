7f9e929d8b75dfc4a8335499c3a0c7ae8dbb9dfc
anshojha <anshojha24@gmail.com>
2023-01-31 22:53:54 +0530

    Learned UseState

--- useState/second/src/App.js
+++ useState/second/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React , {useState} from 'react'
+
+
 
 function App() {
+const [aman , setAman] = useState({count : 0 , theme:'blue'})  //[currentState , updatedState]
+const count = aman.count
+const theme = aman.theme
+
+function decrementCount(){
+setAman(precState => {
+  return {...precState,count : precState.count-1}
+})
+}
+
+function incrementCount (){
+ setAman(precState =>{
+  return {...precState,count:precState.count+1}
+ })
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <button  onClick={decrementCount}>-</button>
+    <span>{count}</span>
+    <span>{theme}</span>
+    <button onClick={incrementCount}>+</button>
+    </>
   );
 }
 
