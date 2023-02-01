5959ea6257686e464be701a887b00f588593c4ef
anshojha <anshojha24@gmail.com>
2023-02-01 23:42:09 +0530

    Started 2nd React with help of of Youtube

---
+++ ReactL/GoodBye CDN/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const navbar = (
+    <nav>
+        <h1>Bob's restro</h1>
+        <ul>
+            <li>Menu</li>
+            <li>Contact</li>
+            <li>About</li>
+        </ul>
+    </nav>
+
+)
+
+ReactDom.render(navbar, document.getElementById="root")
