75210842feef4e39584c097a477601b908c34f96
anshojha <anshojha24@gmail.com>
2023-02-15 23:31:33 +0530

    Product Component

--- amazone-clone/src/App.js
+++ amazone-clone/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
+import Header from './Header' ;
+import Home from './Home';
 function App() {
   return (
     <div className="app">
       {/* <h1>Hello This the form the app js 🚀 </h1> */}
       <Header/>
-      {/* Home.js */}
+      <Home/>
     </div>
   );
 }
