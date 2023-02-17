0ff2ffd01f3e1525c42782635dc16cb431f3a761
anshojha <anshojha24@gmail.com>
2023-02-17 22:17:03 +0530

    Eror in Render

--- amazone-clone/src/App.js
+++ amazone-clone/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import './App.css';
-import Header from './Header' ;
-import Home from './Home';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
+// import {Switch } from "react-router-dom";
 function App() {
   return (
+    <>
     <div className="app">
-      <Header/>
-      <Home/>
-    </div>
+    <Routes>
+    
+        <Route path="/">
+          <Header/>
+          <Home />
+        </Route>
+  
+    </Routes>
+  </div>
+  </>
   );
 }
 
