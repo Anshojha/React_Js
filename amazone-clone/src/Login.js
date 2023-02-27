1321b71a98df400314e71e1a2af0ed5005e4369e
Ansh Ojha <anshojha24@gmail.com>
2023-02-27 22:36:51 +0530

    Having dificulty in firebase

--- amazone-clone/src/Login.js
+++ amazone-clone/src/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Login.css'
+import {auth} from  './firebase'
 function Login() {
   const [email , setEmail] = useState('');
   const [password , setPassword] = useState('');
--- amazone-clone/src/firebase.js
+++ amazone-clone/src/firebase.js
@@ -1,4 +1,9 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import firebase from './firebase'
+
+import 'firebase/auth';
+import 'firebase/firestore';
 const firebaseConfig = {
     apiKey: "AIzaSyDQ--sWgeCgVQKGXQh18Q-MU0YMsuMS8dg",
     authDomain: "e-clone-a567d.firebaseapp.com",
@@ -8,4 +13,12 @@ const firebaseConfig = {
     appId: "1:52559770824:web:4211a1002de9d17bf821fd",
     measurementId: "G-DT60PCTZZS"
   };
+
   
+  const firebaseApp = firebase.initializeApp(firebaseConfig) ;
+
+  const db = firebaseApp.firestore();
+
+  const auth = firebase.auth();
+
+  export {db , auth};
