e36522d391e9066bad32f6cc68ac0ce4ea5cab94
Ansh Ojha <anshojha24@gmail.com>
2023-03-01 23:35:51 +0530

    Added constructor

--- newsmonkey/src/components/News.js
+++ newsmonkey/src/components/News.js
@@ -9,9 +9,20 @@ export class News extends Component {
 
   render() {
     return (
-      <div>
-        This is a newsComponent
-        <NewsItems/>
+      <div className='container my-3 '>
+        <h2>NewsMonkey - Top headline</h2>
+        <div className="row">
+          <div className="col-md-4">
+        <NewsItems title="myTitle" description="myDesc" imageUrl="https://i.guim.co.uk/img/media/a824385b3bc6aca2b3c700437c055ea804dc053c/0_204_4303_2583/master/4303.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdGctZGVmYXVsdC5wbmc&enable=upscale&s=901c8a99b0ec753163cba3988a84bc63"/>
+          </div>
+          <div className="col-md-4">
+        <NewsItems title="myTitle" description="myDesc"/>
+          </div>
+          <div className="col-md-4">
+        <NewsItems title="myTitle" description="myDesc"/>
+          </div>
+        </div>
+  
       </div>
     )
   }
--- newsmonkey/src/components/NewsItems.js
+++ newsmonkey/src/components/NewsItems.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItems extends Component {
   render() {
+    let {title , description , imageUrl} = this.props;
     return (
-      <div>
-        I am from Newa Item
+      <div className="my-3">
+        <div className="card" style={{width: "18rem"}}>
+          <img src={imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">
+             {description}
+            </p>
+            <a href="#" className="btn btn-sm btn-primary">
+              Read More
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItems
+export default NewsItems;
