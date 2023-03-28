a51f8daf2bc2348f2bebf21927ba6dbe341c8e08
Ansh Ojha <anshojha24@gmail.com>
2023-03-28 23:08:20 +0530

    Started the Backend part of Clone

--- amazone-clone/functions/index.js
+++ amazone-clone/functions/index.js
@@ -1,8 +1,36 @@
 const functions = require("firebase-functions");
 
+const express = require('express');
+const cors = require('cors');
+const stripe = require('stripe')('sk_test_51Mn4P7SAl5gguNQ4sCucZqfVnhC63y0cmqLSBlRPhSDPQeGaaaLtQR10DVLomuhxWqVZXkwMXf825mt94bTusio900UdSnPSRd');
+
+// API
+const app = express();
+
+
+// App Config
+
+app.use(cors({origin : true }));
+app.use(express.json());
+
+// Api routes 
+
+
+app.get('/', (request , response)=> response.status(200).send('Jai Shree Ram'))
+
+
+// Middle Wares
 
 
 
+
+//Listen Command
+
+
+exports.api = functions.https.onRequest(app);
+
+// Example end point
+// http://127.0.0.1:5001/e-clone-a567d/us-central1/api
 
 
 
