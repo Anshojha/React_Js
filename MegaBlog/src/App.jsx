39f6cbdcd92a5ef3eb21190b3d9f900642a5ead0
Ansh Ojha <anshojha24@gmail.com>
2024-04-06 08:11:13 +0530

    Store.js to use reducers

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,9 +1,23 @@
-
+import React , {useState , useEffect } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import { login , logout } from './conf/store/authSlice'
 
 function App() {
 
-console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading , setLoading] = useState(false)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login(userData));
+      }
+    })
+    .finally()
+  },[]);
   return (
     <>
      <h1>A Blog with appwrite</h1>
