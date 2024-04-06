eb56cb53203d9d85cd011b6963dc99006e3dabb8
Ansh Ojha <anshojha24@gmail.com>
2024-04-06 19:49:11 +0530

    Added Tailwind and provider

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,28 +1,37 @@
-import React , {useState , useEffect } from 'react'
-import {useDispatch} from 'react-redux'
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
 import authService from './appwrite/auth'
-import { login , logout } from './conf/store/authSlice'
+import { login, logout } from './store/authSlice'
+import { Footer, Header } from './components'
 
 function App() {
 
-  const [loading , setLoading] = useState(false)
+  const [loading, setLoading] = useState(false)
   const dispatch = useDispatch()
 
-  useEffect(()=>{
+  useEffect(() => {
     authService.getCurrentUser()
-    .then((userData)=>{
-      if(userData){
-        dispatch(login(userData));
-      }
-      else{
-        dispatch(logout)
-      }
-    })
-    .finally(() => setLoading(false))
-  },[]);
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        }
+        else {
+          dispatch(logout)
+        }
+      })
+      .finally(() => setLoading(false))
+  }, []);
   return !loading ? (
-    <div className='min-h-screen flex  flex-wrap'></div>
+    <div className='min-h-screen flex  flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO :   {/* <Outlet/> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
   ) : null
 }
 
