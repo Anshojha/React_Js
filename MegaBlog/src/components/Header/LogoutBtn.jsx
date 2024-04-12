5d2f82b4b95c16c1a56b791ffc8a419e773a6333
Ansh Ojha <anshojha24@gmail.com>
2024-04-12 20:54:12 +0530

    Added common button and passed different rander methods to the common button

--- MegaBlog/src/components/Header/LogoutBtn.jsx
+++ MegaBlog/src/components/Header/LogoutBtn.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import {useDispatch} from 'react-redux'
-import authService from '../../appwrite/auth'
+import authService from '../../appwrite/config'
 import {logout} from  '../../store/authSlice'
 
+
 function LogoutBtn() {
-    const dipactch  = useDispatch()
+    const dispatch  = useDispatch()
+    const logoutHandler = () => {
+      authService.logout().then(()=>{
+        dispatch(logout())
+      })
+    }
   return (
-    <div>
-      
-    </div>
+    <button className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>Logout</button>
   )
 }
 
