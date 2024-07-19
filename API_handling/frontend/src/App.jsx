e3dec867883d804202f21903423c761d4b7e2e31
Ansh Ojha <anshojha24@gmail.com>
2024-07-19 13:39:33 +0530

    FO

--- API_handling/frontend/src/App.jsx
+++ API_handling/frontend/src/App.jsx
@@ -1,26 +1,67 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import axios from 'axios'
 
 function App() {
   
-  const [products, error, loading] = fetchQuery('/api/products')
+  // const [products, error, loading] = fetchQuery('/api/products')
+
+  const [products, setProducts] = useState([])
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    const controller = new AbortController()
+    ;(async () => {
+      try {
+        setLoading(true)
+        setError(false)
+        const repone = await axios.get('/api/products?search=' + search, {
+          signal : controller.signal
+        })  
+        console.log(repone.data)
+        setProducts(repone.data)
+        setLoading(false)
+      } catch (error) {
+        if(axios.isCancel(error)) {
+          console.log("Request Cancel ", error.message)
+          return
+        }
+        setError(true)
+
+        // after every call we need to unmount the data which has beeb mnounted or loaded data
+
+        return () => {
+          controller.abort()
+        }
+      }
+    })()
+  }, [search])
 
 
-  if(error) {
-    return <h1>Somthing went wrong</h1>
-  }
+  // if(error) {
+  //   return <h1>Somthing went wrong</h1>
+  // }
 
-  if(loading) {
-    return <h1>....Loading      </h1>
-  }
+  // if(loading) {
+  //   return <h1>....Loading      </h1>
+  // }
 
   return (
     <>
      <h1>Handle chai with API</h1>
-     <h2>No of products {products.length}</h2>
+
+     <input placeholder='search' type='text'
+     value={search}
+     onChange={(e) => setSearch(e.target.value)}
+     />
+     
+    {loading && (<h1>Loading.....</h1>)}
+    {error && (<h1>Somthing went wrong</h1>)}
+
+    <h2>No of products {products.length}</h2>
     </>
   )
 }
@@ -29,24 +70,7 @@ export default App
 
 
 const fetchQuery = () => {
-  const [products, setProducts] = useState([])
-  const [error, setError] = useState(false)
-  const [loading, setLoading] = useState(false)
-
-  useEffect(() => {
-    (async () => {
-      try {
-        setLoading(true)
-        setError(false)
-        const repone = await axios.get('/api/products')  
-        console.log(repone.data)
-        setProducts(repone.data)
-        setLoading(false)
-      } catch (error) {
-        setError(true)
-      }
-    })()
-  }, [])
+ 
 
   return [products , error , loading]
 }
