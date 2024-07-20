9bf9774c0af9e6b52b1ad46f4706f2392519dc37
Ansh Ojha <anshojha24@gmail.com>
2024-07-20 19:38:32 +0530

    Added navbar and home component

---
+++ Order Food/frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='header-contents'>
+            <h2>Choose your favourite food here</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quae dolores labore, corporis vitae deleniti quod ab culpa eveniet ratione? Eaque vel animi dolorum voluptate ex reiciendis debitis ipsa odit?</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
