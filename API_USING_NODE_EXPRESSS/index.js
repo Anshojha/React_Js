e473ef43250bf2e90aade6991cc3ea9b8a0ed6cd
Ansh Ojha <anshojha24@gmail.com>
2024-08-04 01:23:27 +0530

    API

--- API_USING_NODE_EXPRESSS/index.js
+++ API_USING_NODE_EXPRESSS/index.js
@@ -2,6 +2,8 @@ import express from "express";
 
 import bodyParser from "body-parser";
 
+import userRoutes from './routes/users.js'
+
 const app = express();
 
 const PORT = 5000;
@@ -11,6 +13,8 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("Hello from the home page");
 });
+
+app.use('/users', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is Running on port : http://localhost:${PORT}`);
