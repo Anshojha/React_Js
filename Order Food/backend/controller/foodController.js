e1d61c09de8c11108ce0ff40576b95e9e8b1a00b
Ansh Ojha <anshojha24@gmail.com>
2024-09-07 00:06:35 +0530

    backend

---
+++ Order Food/backend/controller/foodController.js
@@ -0,0 +1,8 @@
+import foodModel from "../models/foodModels.js";
+import fs from "fs";
+
+const addFood =  async (req, res) => {
+    let image_filename = `${req.file.filename}`
+}
+
+export {addFood};   
---
+++ Order Food/backend/server.js
@@ -0,0 +1,31 @@
+import express from "express"
+import cors from "cors"
+import {connectDB} from "./config/db.js"
+import foodRouter from "./routes/foodRoutes.js";
+
+// app- config
+const app = express()
+const port = 4000;
+
+// middleware
+app.use(express.json()) 
+app.use(cors())
+
+// db connection
+connectDB();
+
+// API endpoints
+
+app.use("/api/foos",foodRouter)
+
+app.get("/", (req, res)=>{
+    res.send("API is working")
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is working on the http://localhost:${port}`)
+})
+
+
+
+// mongodb+srv://ansh:9838698121@cluster0.1pzbu.mongodb.net/?
