165612045344552a3e76f979ece40eab01aa032e
Ansh Ojha <anshojha24@gmail.com>
2024-09-07 18:47:37 +0530

    added remove from item

--- Order Food/backend/controller/foodController.js
+++ Order Food/backend/controller/foodController.js
@@ -34,6 +34,18 @@ const listFood = async (req, res) => {
     }
 }
 
+// To remove foos item from the database
 
+const removeFood = async (req, res) => {
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`, () => {})
+        await foodModel.findByIdAndDelete(req.body.id)
+        res.json({success : true, message : "Food removed"})
+    } catch (error) {
+        console.log({success:false , message : "Error"})
+    }
+    
+}
 
-export {addFood, listFood};   
+export {addFood, listFood, removeFood};   
